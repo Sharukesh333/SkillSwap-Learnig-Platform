4a266d0dda6bb852e1bbdef466419e12cc97129c
Sharukesh Ganesan <sharukeshkamal@gmail.com>
2026-06-25 20:20:39 +0530

    Fix Vercel json-server path

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("./db.json");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const router = jsonServer.router(
+  path.join(__dirname, "../db.json")
+);
 
 const middlewares = jsonServer.defaults();
 
